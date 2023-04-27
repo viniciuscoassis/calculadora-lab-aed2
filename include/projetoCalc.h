27ca66bdaacaf3eb046b14ec35a23caa895e935a
Vinicius Assis <106977892+viniciuscoassis@users.noreply.github.com>
2023-04-26 22:06:06 -0300

    feat: add include

---
+++ include/projetoCalc.h
@@ -0,0 +1,8 @@
+// Definicoes e prototipos
+void clearscr(void);
+float soma(float, float);
+float subtracao(float, float);
+float multiplicacao(float, float);
+float divisao(float, float);
+char operador();
+int desejarContinuar();
